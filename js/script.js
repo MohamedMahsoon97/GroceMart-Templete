3faf9683ce2cd73ea994bbe9bb3e07b2ccf7ca22
MohamedMahsoon97 <mohamedmahsoon61@gmail.com>
2021-07-27 20:20:38 +0200

    finish task

--- js/script.js
+++ js/script.js
@@ -60,29 +60,91 @@ decrement.onclick = function() {
 //-----------  start animation in about section  -----------//
 window.onscroll = function() {
     const myNavbar = document.getElementById("nav");
+    const myheaderSlider = document.querySelector("div.headerSlider");
     const aboutContent = document.getElementById("about-content");
     const parallexContent = document.getElementById("parallax-content");
+    const myProdTitle1 = document.getElementById("title-pro-1")
+    const myProdTitle2 = document.getElementById("title-pro-2")
+    const myTestTitle1 = document.getElementById("title-test-1")
+    const myTestTitle2 = document.getElementById("title-test-2")
+    const myProductsCards = document.getElementById("products-card")
+    const mySaleImg = document.querySelector("div.sale");
     const toTopPage = document.getElementById("to-top");
-    // if(window.pageYOffset >= 100) {
-    //     myNavbar.classList.add("nav-fixed");
-    // };
-    if(window.pageYOffset >= 380) {
+    if (true) {
+        if (window.pageYOffset >= 200) {
+            myNavbar.classList.add("nav-fixed");
+            myheaderSlider.style.marginTop = "100px"
+        } else {
+            myNavbar.classList.remove("nav-fixed");
+            myheaderSlider.style.marginTop = "0px"
+        }
+    }
+    if(window.pageYOffset >= 200) {
+        myNavbar.classList.add("nav-fixed");
+        myheaderSlider.style.marginTop = "100px"
+    };
+    if(window.pageYOffset >= 350) {
         aboutContent.classList.add("make-animation-about")
     };
     if(window.pageYOffset >= 850) {
         parallexContent.classList.add("parallax-animation");
     }
+    if(window.pageYOffset >= 1200) {
+        myProdTitle1.classList.add("products-animation-1");
+        myProdTitle2.classList.add("products-animation-2");
+    }
+    if(window.pageYOffset >= 1400) {
+        myProductsCards.style.opacity = "1";
+    }
+    if(window.pageYOffset >= 2000) {
+        myTestTitle1.classList.add("products-animation-1");
+        myTestTitle2.classList.add("products-animation-2");
+    }
+    if(window.pageYOffset >= 2730) {
+        mySaleImg.childNodes[1].style.animationName = "sale-img1";
+        mySaleImg.childNodes[1].style.opacity = 1;
+        mySaleImg.childNodes[3].style.animationName = "sale-img2";
+        mySaleImg.childNodes[3].style.opacity = 1;
+    }
     if(true){
         if(window.pageYOffset >= 850) {
-            toTopPage.style.opacity = "1"
+            toTopPage.classList.add("bt-to-top")
         }else{
-            toTopPage.style.opacity = "0"
+            toTopPage.classList.remove("bt-to-top")
         }
     }
-
 }
 //-----------  end animation in about section  ------------//
 
-//-----------  Start animation in parallex section  -----------//
+//---------- start animation label input in footer -----------//
+const inpFormFooter = document.getElementById("inp-form");
+const labelFormFooter = document.getElementById("label");
+inpFormFooter.addEventListener('focus' , function() {
+    labelFormFooter.style.top = "150px";
+    labelFormFooter.style.fontSize = "14px";
+    labelFormFooter.style.color = "#7fd02c";
+})
+inpFormFooter.addEventListener('blur' , function() {
+    labelFormFooter.style.top = "170px"
+    labelFormFooter.style.fontSize = "17px";
+    labelFormFooter.style.color = "#000";
+})
+//--------- end animation label input in footer ------------//
 
-//-----------  End animation in parallex section  -----------//
+//--------- Start accordion (Common-Questions-FAQ) ---------//
+const Questions = document.querySelectorAll(".Question");
+const spanClick = document.querySelectorAll(".ask-icon");
+Questions.forEach ( FAQ => {
+    FAQ.addEventListener('click' , () => {
+        FAQ.children[1].classList.toggle("active");
+        spanClick.classList.add("span-click");
+    })
+})
+//--------- End accordion (Common-Questions-FAQ) ---------//
+
+//-----------  Start to Top ------------//
+// const toTopPage = document.getElementById("to-top");
+// toTopPage.addEventListener('click' , function() {
+//     window.pageYOffset = "0";
+// })
+//-----------  End to Top ------------//
